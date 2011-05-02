b5bf8e102adb21ae5c72ce7a06dfec3ec4dd4176
Bernardo Heynemann <heynemann@gmail.com>
2011-05-01 22:05:17 -0300

    Added more multiple topic tests

---
+++ tests/multiple_topic_vows.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from pyvows import Vows, expect
+
+@Vows.batch
+class MultipleTopics(Vows.Context):
+    class FirstLevel(Vows.Context):
+        def topic(self):
+            return 'a'
+
+        def is_a(self, topic):
+            expect(topic).to_equal('a')
+
+        class SecondLevel(Vows.Context):
+            def topic(self, first):
+                return (first, 'b')
+
+            def still_a(self, topic):
+                expect(topic[0]).to_equal('a')
+
+            def is_b(self, topic):
+                expect(topic[1]).to_equal('b')
+
+            class ThirdLevel(Vows.Context):
+                def topic(self, second, first):
+                    return (first, second[1], 'c')
+
+                def still_a(self, topic):
+                    expect(topic[0]).to_equal('a')
+
+                def still_b(self, topic):
+                    expect(topic[1]).to_equal('b')
+
+                def is_c(self, topic):
+                    expect(topic[2]).to_equal('c')
+
+
