a7bcb54e177e28c4b8f9d1f15ed68f8b24284b7d
Zearin <zearin@gonk.net>
2012-10-21 14:47:45 -0400

    Tweaked output of `--help`
    * Added meta-vars to arg descriptions
    * Use `str.format` where possible (can’t get it working in the help strings in `Messages`)

--- pyvows/console.py
+++ pyvows/console.py
@@ -22,56 +22,73 @@ try:
 except ImportError:
     COVERAGE_AVAILABLE = False
 
-from pyvows.color import Fore
+from pyvows.color import Fore, Style
 from pyvows.xunit import XUnitReporter
 from pyvows import version
 
 
 class Messages(object):
-    pattern = 'Pattern of vows files. (default: %(default)r).'
-    progress = 'Indicates that progress ticks should be shown. (default: %(default)s).'
-    cover = 'Indicates that coverage of code should be shown. (default: %(default)s).'
-    cover_package = 'Package to verify coverage. May be specified many times. (default: all packages).'
-    cover_omit = 'Path of file to exclude from coverage. May be specified many times. (default: no files).'
-    cover_threshold = 'Coverage number below which coverage is considered failing. (default: %(default)s).'
-    cover_report = 'Store the coverage report as the specified file. (default: %(default)s).'
-    xunit_output = 'Enable XUnit output. (default: %(default)s).'
-    xunit_file = 'Filename of the XUnit output (default: %(default)s).'
-    no_color = 'Does not colorize the output. (default: %(default)s).'
-    verbosity = 'Verbosity. Can be supplied multiple times to increase verbosity (default: -vv)'
-    path = 'Directory to look for vows recursively. If a file is passed, the file will be the target for vows. (default: %(default)r).'
-    profile = 'Prints the 10 slowest topics. (default: %(default)s).'
-    profile_threshold = 'Number of seconds that the test must take to be considered a slow test. (default: %(default)s).'
+    path      = 'Directory to look for vows recursively. If a file is passed, the file will be the target for vows. (default: %(default)r).'
+    pattern   = 'Pattern of vows files. (default: %(default)r)'
+    verbosity = 'Verbosity. May be specified many times to increase verbosity (default: -vv)'
+    
+    cover           = 'Show the code coverage of tests. (default: %(default)s)'
+    cover_package   = 'Verify coverage of %(metavar)s. May be specified many times. (default: all packages)'
+    cover_omit      = 'Exclude %(metavar)s from coverage. May be specified many times. (default: no files)'
+    cover_threshold = 'Coverage below %(metavar)s is considered a failure. (default: %(default)s)'
+    cover_report    = 'Store coverage report as %(metavar)s. (default: %(default)r)'
+    
+    xunit_output    = 'Enable XUnit output. (default: %(default)s)'
+    xunit_file      = 'Store XUnit output as %(metavar)s. (default: %(default)r)'
+    
+    profile           = 'Prints the 10 slowest topics. (default: %(default)s)'
+    profile_threshold = 'Tests taking longer than %(metavar)s seconds are considered slow. (default: %(default)s)'
+    
+    no_color  = 'Turn off colorized output. (default: %(default)s)'
+    progress  = 'Show progress ticks during testing. (default: %(default)s)'
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 def __get_arguments():
     current_dir = os.curdir
+    
+    uline   = lambda text: '\033[4m{0}\033[24m'.format(text)
+    metavar = lambda metavar: '{0}{1}{2}'.format(Style.RESET_ALL, metavar.upper(), Style.RESET_ALL)
+    
+    parser = argparse.ArgumentParser(description     = 'Runs pyVows.')
 
-    parser = argparse.ArgumentParser(description='Runs pyVows.')
+    parser.add_argument('-p', '--pattern', default='*_vows.py', help=Messages.pattern, metavar=metavar('pattern'))
 
-    parser.add_argument('-p', '--pattern', default='*_vows.py', help=Messages.pattern)
+    cover_group = parser.add_argument_group('Test Coverage')
+    cover_group.add_argument('-c', '--cover',           action='store_true', default=False, help=Messages.cover)
+    cover_group.add_argument('-l', '--cover_package',   action='append',     default=[],    help=Messages.cover_package, metavar=metavar('package'))
+    cover_group.add_argument('-o', '--cover_omit',      action='append',     default=[],    help=Messages.cover_omit,    metavar=metavar('file'))
+    cover_group.add_argument('-t', '--cover_threshold', type=float,          default=80.0,  help=Messages.cover_threshold, metavar=metavar('number'))
+    cover_group.add_argument('-r', '--cover_report',    action='store',      default=None,  help=Messages.cover_report, metavar=metavar('file'))
 
-    cover_group = parser.add_argument_group('coverage arguments')
-    cover_group.add_argument('-c', '--cover', action="store_true", default=False, help=Messages.cover)
-    cover_group.add_argument('-l', '--cover_package', action="append", default=[], help=Messages.cover_package)
-    cover_group.add_argument('-o', '--cover_omit', action="append", default=[], help=Messages.cover_omit)
-    cover_group.add_argument('-t', '--cover_threshold', default=80.0, type=float, help=Messages.cover_threshold)
-    cover_group.add_argument('-r', '--cover_report', action="store", default=None, help=Messages.cover_report)
+    xunit_group = parser.add_argument_group('XUnit')
+    xunit_group.add_argument('-x', '--xunit_output', action='store_true', default=False,        help=Messages.xunit_output)
+    xunit_group.add_argument('-f', '--xunit_file',   action='store',      default='pyvows.xml', help=Messages.xunit_file, metavar=metavar('file'))
 
-    xunit_group = parser.add_argument_group('xunit arguments')
-    xunit_group.add_argument('-x', '--xunit_output', action="store_true", default=False, help=Messages.xunit_output)
-    xunit_group.add_argument('-f', '--xunit_file', action="store", default="pyvows.xml", help=Messages.xunit_file)
-
-    profile_group = parser.add_argument_group('profile arguments')
+    profile_group = parser.add_argument_group('Profiling')
     profile_group.add_argument('--profile', action='store_true', dest='profile', default=False, help=Messages.profile)
-    profile_group.add_argument('--profile_threshold', default=0.1, type=float, help=Messages.profile_threshold)
+    profile_group.add_argument('--profile_threshold', type=float,                default=0.1,   help=Messages.profile_threshold, metavar=metavar('num'))
 
-    parser.add_argument('--no_color', action="store_true", default=False, help=Messages.no_color)
+    parser.add_argument('--no_color', action='store_true',                  default=False, help=Messages.no_color)
     parser.add_argument('--progress', action='store_true', dest='progress', default=False, help=Messages.progress)
 
-    parser.add_argument('--version', action='version', version='%(prog)s ' + version.to_str())
-    parser.add_argument('-v', action='append_const', dest='verbosity', const=1, help=Messages.verbosity)
+    parser.add_argument('--version', action='version', version='%(prog)s {0}'.format(version.to_str()))
+    parser.add_argument('-v',        action='append_const', dest='verbosity', const=1, help=Messages.verbosity)
 
-    parser.add_argument('path', default=current_dir, nargs='?', help=Messages.path)
+    parser.add_argument('path', nargs='?', default=current_dir, help=Messages.path)
 
     arguments = parser.parse_args()
 
@@ -120,8 +137,7 @@ def main():
         if COVERAGE_AVAILABLE:
             cov.stop()
 
-            print
-            print
+            print '\n' * 2
 
             xml = ''
             with tempfile.NamedTemporaryFile() as tmp:
@@ -138,8 +154,8 @@ def main():
         else:
 
             print
-            print Fore.YELLOW + "WARNING: Cover disabled because coverage could not be found."
-            print Fore.YELLOW + "Make sure it is installed and accessible"
+            print '{0.YELLOW}WARNING: Cover disabled because coverage could not be found.'.format(Fore)
+            print '{0.YELLOW}Make sure it is installed and accessible.'.format(Fore)
             print
 
     reporter.pretty_print()
