23500134f8071d72a0db7b3f4fecc519fa4eb91b
Bernardo Heynemann <heynemann@gmail.com>
2012-10-25 11:46:01 -0200

    SetupTearDownSpecs

--- tests/setup_teardown_vows.py
+++ tests/setup_teardown_vows.py
@@ -15,32 +15,40 @@ class SetupTeardownSpecs(Vows.Context):
     exec_order = []
 
     def teardown(self):
-        expect(self.exec_order).to_equal([1,2,3,-2,-1])
+        print '1st level teardown'
+        expect(self.exec_order).to_equal([1, 2, 3, -2, -1])
 
     class OrderOfExecution(Vows.Context):
         def setup(self):
+            print '2nd level setup'
             self.parent.exec_order.append(1)
 
         def topic(self):
+            print '2nd level topic'
             self.parent.exec_order.append(2)
             return 20
 
         def teardown(self):
+            print '2nd level teardown'
             self.parent.exec_order.append(-1) #last
-            expect(self.parent.exec_order).to_equal([1,2,3,-2,-1])
+            expect(self.parent.exec_order).to_equal([1, 2, 3, -2, -1])
 
         def check_order(self, topic):
+            print '2nd level expectation'
             expect(self.parent.exec_order).to_equal([1, 2])
 
         class TeardownOrderOfExecution(Vows.Context):
             def setup(self):
+                print '3rd level setup'
                 self.parent.parent.exec_order.append(3)
 
             def teardown(self):
+                print '3rd level teardown'
                 self.parent.parent.exec_order.append(-2) #right before the parent's teardown
-                expect(self.parent.parent.exec_order).to_equal([1,2,3,-2])
+                expect(self.parent.parent.exec_order).to_equal([1, 2, 3, -2])
 
             def check_order(self, topic):
+                print '3rd level expectation'
                 expect(self.parent.parent.exec_order).to_equal([1, 2, 3])
 
 
