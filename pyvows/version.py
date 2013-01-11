c534d040395d73de5da3a34b5c9ddf2c7260e67f
Bernardo Heynemann <heynemann@gmail.com>
2013-01-11 15:05:13 -0200

    Release 1.1.11

--- pyvows/version.py
+++ pyvows/version.py
@@ -10,7 +10,7 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
-__version__ = (1, 1, 10)
+__version__ = (1, 1, 11)
 
 def to_str():
     '''Returns a string containing PyVows' version number.'''
