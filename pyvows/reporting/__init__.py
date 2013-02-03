adf03a70725f63c23cbe24548fbbba7d3dbba45a
Zearin <zearin@gonk.net>
2013-02-03 11:55:52 -0500

    Made reporting a package

---
+++ pyvows/reporting/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# pyvows testing engine
+# https://github.com/heynemann/pyvows
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license
+# Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
+
+import pyvows.reporting.reporting
+from pyvows.reporting.reporting import VowsTestReporter, VowsDefaultReporter
+
+
+__all__ = [ 'VowsTestReporter',
+            'VowsDefaultReporter',
+    ]
