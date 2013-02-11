80ae0e031816babbf30d1c8c253bdc6ddb818ec4
Zearin <zearin@gonk.net>
2013-02-11 12:34:33 -0500

    Moved `locate()` from module `core` to `util`

--- pyvows/utils.py
+++ pyvows/utils.py
@@ -9,8 +9,28 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
+import fnmatch
+import glob
+import os
 
 from pyvows.errors  import _AssertionNotFoundError, VowsAssertionError
+
+
+def locate(pattern, root=os.curdir, recursive=True):
+    '''Recursively locates test files when `pyvows` is run from the
+    command line.
+    
+    '''
+    root_path = os.path.abspath(root)
+
+    if recursive:
+        return_files = []
+        for path, dirs, files in os.walk(root_path):
+            for filename in fnmatch.filter(files, pattern):
+                return_files.append(os.path.join(path, filename))
+        return return_files
+    else:
+        return glob(os.path.join(root_path, pattern))
 
 
 class VowsAssertion(object):
