408a6c9f355f8c89c9a1e1808d0bf698782368f4
Zearin <zearin@gonk.net>
2013-02-11 11:51:02 -0500

    Moved `FunctionWrapper` class to `decorators` module

--- pyvows/decorators.py
+++ pyvows/decorators.py
@@ -9,7 +9,10 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
+from functools import wraps
+
 from pyvows.async_topic import VowsAsyncTopic
+
 
 def async_topic(topic):
     '''Topic decorator.  Allows PyVows testing of asynchronous topics.
@@ -33,3 +36,32 @@ def _batch(method):
         method(*args, **kw)
 
     return method_name
+    
+    
+class FunctionWrapper(object):
+    '''Function decorator.  Simply calls the decorated function when all 
+    the wrapped functions have been called.
+
+    '''
+    def __init__(self, func):
+        self.waiting = 0
+        self.func = func
+
+    def wrap(self, method):
+        self.waiting += 1
+
+        @wraps(method)
+        def wrapper(*args, **kw):
+            try:
+                ret = method(*args, **kw)
+                return ret
+            finally:
+                self.waiting -= 1
+                self()
+
+        wrapper._original = method
+        return wrapper
+
+    def __call__(self):
+        if self.waiting == 0:
+            self.func()
