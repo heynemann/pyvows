c3e2ff4830a9d830b9424bcfb5585df6ad042473
Zearin <zearin@gonk.net>
2013-05-22 14:18:56 -0400

    setup.py: minor tweaks

--- setup.py
+++ setup.py
@@ -8,53 +8,75 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
+# stdlib
+import sys
+from textwrap import dedent
+# external
 from setuptools import setup
+# local
 from pyvows import version
 
+
+_install_requires = [
+    'gevent>=0.13.6',
+    'preggy>=0.5.8',
+]
+if sys.version_info < (2,7):
+    _install_requires.append('argparse >= 1.1')
+      
+        
 setup(
-    name = 'pyVows',
-    version = version.to_str(),
-    description = 'pyVows is a BDD test engine based on Vows.js <http://vowsjs.org>.',
-    long_description = '''
-pyVows is a test engine based on Vows.js. It features topic-based testing, 
-(*fast*) parallel running of tests, code coverage reports, test profiling, and 
-more: http://pyvows.org 
-''',
-    keywords = 'test testing vows tdd bdd development coverage profile profiling',
-    author = 'Bernardo Heynemann',
-    author_email = 'heynemann@gmail.com',
-    #Contributors
-    #contributor = 'Rafael Carício',
-    #contributor_email = 'rafael@caricio.com',
-    url = 'http://github.com/heynemann/pyvows/',
-    license = 'MIT',
-    classifiers = ['Development Status :: 4 - Beta',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Natural Language :: English',
-                   'Operating System :: MacOS',
-                   'Operating System :: POSIX',
-                   'Operating System :: Unix',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
-                   'Topic :: Software Development :: Testing'
-    ],
-    packages = ['pyvows', 'pyvows.reporting'],
-    package_dir = {'pyvows': 'pyvows'},
-
-    install_requires=[
-        'argparse',
-        'gevent>=0.13.6',
-        'preggy>=0.5.8',
-    ],
-
-    entry_points = {
+    ### OVERVIEW
+    name='pyVows',
+    description='pyVows is a BDD test engine based on Vows.js <http://vowsjs.org>.',
+    long_description=dedent(
+        '''pyVows is a test engine based on Vows.js. It features topic-based testing, 
+        (*fast*) parallel running of tests, code coverage reports, test profiling, and 
+        more: http://pyvows.org 
+        '''),
+    
+    
+    ### URLs
+    url='http://pyvows.org',
+    
+    
+    ### technical_info
+    version=version.to_str(),
+    install_requires=_install_requires,
+    packages=['pyvows', 'pyvows.reporting'],
+    package_dir={'pyvows':'pyvows'},
+    entry_points={
         'console_scripts': [
             'pyvows = pyvows.cli:main'
-        ],
+        ]
     },
-
+    
+    
+    ### PEOPLE & LICENSE
+    author='Bernardo Heynemann',
+    author_email='heynemann@gmail.com',
+    #maintainer = 'Rafael Carício',
+    #maintainer_email = 'rafael@caricio.com',
+    maintainer='Zearin',
+    license='MIT',
+    
+    
+    ### CATEGORIZATION 
+    keywords = 'test testing vows tdd bdd development coverage profile profiling',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Testing'
+    ],
+    
 )
 
 
