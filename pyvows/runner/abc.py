35aec56cfed807f9a97f92ce169b419c37b80c02
Zearin <zearin@gonk.net>
2013-06-04 16:14:12 -0400

    dev: chg: Combined "on_vow_success" and "on_vow_error" into "on_vow_hooks"

--- pyvows/runner/abc.py
+++ pyvows/runner/abc.py
@@ -21,17 +21,16 @@ from pyvows.runner.utils import get_code_for, get_file_info_for, get_topics_for
 class VowsRunnerABC(object):
     result = VowsResult()
     
-    def __init__(self, suites, context_class, on_vow_success, on_vow_error, exclusion_patterns):
+    def __init__(self, suites, context_class, on_vow_hooks, exclusion_patterns=None):
         self.suites = suites  # a suite is a file with pyvows tests
         self.context_class = context_class
-        self.on_vow_success = on_vow_success
-        self.on_vow_error = on_vow_error
+        self.on_vow_hooks = on_vow_hooks
         self.exclusion_patterns = exclusion_patterns
         if self.exclusion_patterns:
             self.exclusion_patterns = set([re.compile(x) for x in self.exclusion_patterns])
     
     def _get_vows_and_subcontexts(self, ctx_obj):
-        special_names = set(['setup', 'teardown', 'topic'])
+        special_names = set(['setup', 'teardown', 'topic', 'ignore'])
         if hasattr(ctx_obj, 'ignored_members'):
             special_names.update(ctx_obj.ignored_members)
 
@@ -88,8 +87,8 @@ class VowsRunnerABC(object):
         try:
             vow_result['result'] = vow(ctx_obj, topic)
             vow_result['succeeded'] = True
-            if self.on_vow_success:
-                self.on_vow_success(vow_result)
+            if self.on_vow_hooks[True]:
+                self.on_vow_hooks[True](vow_result)
 
         except:
             #   FIXME:
@@ -103,8 +102,8 @@ class VowsRunnerABC(object):
                 'value': err_value,
                 'traceback': err_traceback
             }
-            if self.on_vow_error:
-                self.on_vow_error(vow_result)
+            if self.on_vow_hooks[False]:
+                self.on_vow_hooks[False](vow_result)
 
         vow_result['elapsed'] = elapsed(start_time)
         tests_collection.append(vow_result)
