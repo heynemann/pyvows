heynemann/pyvows

commit 59e67374bcfbf7c8c45a4be575e8eae64902c209

topics can be none, we need to validate against that.

pyvows/runner/gevent.py
@@ -14,13 +14,12 @@ from __future__ import absolute_import
 import inspect
 import sys
 import time
-import re
 
 from gevent.pool import Pool
 
 from pyvows.async_topic import VowsAsyncTopic, VowsAsyncTopicValue
 from pyvows.decorators import FunctionWrapper
-from pyvows.runner.utils import get_code_for, get_file_info_for, get_topics_for
+from pyvows.runner.utils import get_topics_for
 from pyvows.result import VowsResult
 from pyvows.utils import elapsed
 from pyvows.runner.abc import VowsRunnerABC, VowsTopicError
@@ -86,7 +85,7 @@ class VowsParallelRunner(VowsRunnerABC):
             # Run setup function
             try:
                 ctx_obj.setup()
-            except Exception as e:
+            except Exception:
                 raise VowsTopicError('setup', sys.exc_info())
 
             # Find & run topic function
@@ -98,11 +97,15 @@ class VowsParallelRunner(VowsRunnerABC):
                 topic_list = get_topics_for(topic_func, ctx_obj)
 
                 start_time = time.time()
+
+                if topic_func is None:
+                    return None
+
                 topic = topic_func(*topic_list)
                 ctx_result['topic_elapsed'] = elapsed(start_time)
                 return topic
 
-            except Exception as e:
+            except Exception:
                 raise VowsTopicError('topic', sys.exc_info())
 
         def _run_tests(topic):
@@ -143,7 +146,7 @@ class VowsParallelRunner(VowsRunnerABC):
                     try:
                         ctx_obj.generated_topic = True
                         topic = ctx_obj.topic_value = list(topic)
-                    except Exception as e:
+                    except Exception:
                         # Actually getting the values from the generator may raise exception
                         raise VowsTopicError('topic', sys.exc_info())
                 else:
@@ -177,7 +180,7 @@ class VowsParallelRunner(VowsRunnerABC):
         def _run_teardown(topic):
             try:
                 teardown()
-            except Exception as e:
+            except Exception:
                 raise VowsTopicError('teardown', sys.exc_info())
 
         #-----------------------------------------------------------------------
@@ -188,7 +191,8 @@ class VowsParallelRunner(VowsRunnerABC):
             # Only run tests & teardown if setup & topic run without errors
             _run_tests(topic)
             _run_teardown(topic)
-        except VowsTopicError as e:
+        except VowsTopicError:
+            e = sys.exc_info()[1]
             ctx_obj.topic_error = e   # is this needed still?
             ctx_result['error'] = e
 

pyvows/version.py
@@ -12,7 +12,7 @@
 
 #-------------------------------------------------------------------------------------------------
 
-__version__ = (2, 0, 5)
+__version__ = (2, 0, 6)
 
 #-------------------------------------------------------------------------------------------------
 

commit ed38e6b1bb8491b1f3460e5e4dad651f20ee2c5b

runner.gevent: Shift None check for topic_func earlier
Avoid:

  Traceback (most recent call last):
    File "/.../pyvows/runner/gevent.py", line 97, in _run_setup_and_topic
      topic_list = get_topics_for(topic_func, ctx_obj)
    File "/.../pyvows/runner/utils.py", line 46, in get_topics_for
      'Function %s does not have a code property' % topic_function)
  RuntimeError: Function None does not have a code property

because VowsParallelRunner._run_setup_and_topic was calling
get_topics_for (which chokes when its topic_function is None) before
checking for a None value.

The fixes a broken check from 59e67374 (topics can be none, we need to
validate against that, 2014-06-02).

pyvows/runner/gevent.py
@@ -94,12 +94,12 @@ class VowsParallelRunner(VowsRunnerABC):
 
             try:
                 topic_func = ctx_obj.topic
+                if topic_func is None:
+                    return None
+
                 topic_list = get_topics_for(topic_func, ctx_obj)
 
                 start_time = time.time()
-
-                if topic_func is None:
-                    return None
 
                 topic = topic_func(*topic_list)
                 ctx_result['topic_elapsed'] = elapsed(start_time)